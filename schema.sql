50acc435d9a3e7a49883b539ca8c2d203c84438d
Lordkaito <cespedesisai88@gmail.com>
2021-12-03 16:30:03 +0100

    join table with specialitazions and vets

--- schema.sql
+++ schema.sql
@@ -39,3 +39,34 @@ ALTER TABLE animals
 
 ALTER TABLE animals
   ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+
+CREATE TABLE vets (
+  id SERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(250),
+  age INT,
+  date_of_graduation date
+);
+
+CREATE TABLE specializations (
+  species_id INT,
+  vet_id INT,
+  CONSTRAINT fk_species
+    FOREIGN KEY(species_id)
+      REFERENCES species(id),
+  CONSTRAINT fk_vets
+    FOREIGN KEY(vet_id)
+      REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+  animals_id INT,
+  vets_id INT,
+  date_of_visit date,
+  CONSTRAINT fk_animals
+    FOREIGN KEY(animals_id)
+      REFERENCES animals(id),
+  CONSTRAINT fk_vets
+    FOREIGN KEY(vets_id)
+      REFERENCES vets(id)
+);
